4ebc0735959478a0f5d30f297e1022c07f9ccd54
Caleb Smith <karagenit@outlook.com>
2024-11-26 21:23:09 -0500

    Add Flask Web Server

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from auth import get_creds
 from tasks import get_tasks
+from flask import Flask
 
-creds = get_creds()
-tasks = get_tasks(creds)
+app = Flask(__name__)
 
-while True:
-    user_input = input("> ")
-    if user_input == "list":
-        for task in tasks:
-            print(f"{task['title']} ({task['priority']})")
-    elif user_input == "today":
-        sorted_tasks = sorted(tasks, key=lambda x: x.get('priority', 0), reverse=True)
-        for task in sorted_tasks[:3]:
-            print(f"{task['title']} ({task['priority']})")
-    else:  # Default case
-        print("Unknown command")
+@app.route('/')
+def index():
+    return "hello world"
+
+app.run(debug=True)
+
+# creds = get_creds()
+# tasks = get_tasks(creds)
+
+# while True:
+#     user_input = input("> ")
+#     if user_input == "list":
+#         for task in tasks:
+#             print(f"{task['title']} ({task['priority']})")
+#     elif user_input == "today":
+#         sorted_tasks = sorted(tasks, key=lambda x: x.get('priority', 0), reverse=True)
+#         for task in sorted_tasks[:3]:
+#             print(f"{task['title']} ({task['priority']})")
+#     else:  # Default case
+#         print("Unknown command")
