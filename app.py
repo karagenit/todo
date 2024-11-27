7409337ad25d187ae709f5c2d7f26dcf65cf1c76
Caleb Smith <karagenit@outlook.com>
2024-11-26 21:43:37 -0500

    List Tasks on Index

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from auth import get_creds
 from tasks import get_tasks
 from flask import Flask, render_template
 
+creds = get_creds()
+tasks = get_tasks(creds)
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', name='test')
+    sorted_tasks = sorted(tasks, key=lambda x: x.get('priority', 0), reverse=True)
+    top_tasks = sorted_tasks[:3]
+    return render_template('index.html', tasks=tasks)
 
 app.run(debug=True)
 
