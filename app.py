62f6e81e1202329e61ed7ffd5f212ad1523c5ebb
Caleb Smith <karagenit@outlook.com>
2024-11-30 20:14:46 -0500

    Cleanup

--- app.py
+++ app.py
@@ -39,18 +39,3 @@ def update_task():
 # TODO button to force reload from api
 
 app.run(debug=True)
-
-# creds = get_creds()
-# tasks = get_tasks(creds)
-
-# while True:
-#     user_input = input("> ")
-#     if user_input == "list":
-#         for task in tasks:
-#             print(f"{task['title']} ({task['priority']})")
-#     elif user_input == "today":
-#         sorted_tasks = sorted(tasks, key=lambda x: x.get('priority', 0), reverse=True)
-#         for task in sorted_tasks[:3]:
-#             print(f"{task['title']} ({task['priority']})")
-#     else:  # Default case
-#         print("Unknown command")
