9c909ebe9eb1cdd29c6aa0aaca18510cc7877a9d
Caleb Smith <karagenit@outlook.com>
2025-04-06 20:26:24 -0400

    Add Future Tasks Filter to UI

--- filter_args.py
+++ filter_args.py
@@ -3,5 +3,5 @@ class FilterArgs:
         # if request_args: # TODO still set default values if request_args was null?
         self.search = request_args.get('search', '')
         self.hide_children = request_args.get('hide_children', True)
-        self.hide_future = request_args.get('hide_future', True)
+        self.hide_future = request_args.get('hide_future', False)
     
